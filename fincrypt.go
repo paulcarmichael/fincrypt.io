902b7b48a408093d68a172d658f92195725565f8
Paul Carmichael <paulcarmo@gmail.com>
2018-04-21 15:24:39 -0700

    serving up simple XML

--- fincrypt.go
+++ fincrypt.go
@@ -1,158 +1,76 @@
 package main
 
 import (
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
 	"os"
-	"strings"
-
-	"github.com/paulcarmichael/cryptop"
+	"path/filepath"
 )
 
-func index(w http.ResponseWriter, r *http.Request) {
+func serve(w http.ResponseWriter, r *http.Request) {
+	// build the string of the template we require based on the request URL
+	var buffer bytes.Buffer
 
-	t, _ := template.ParseFiles("C:\\Users\\Paul\\go\\src\\github.com\\paulcarmichael\\fincrypt\\html\\index.html")
+	buffer.WriteString("../src/github.com/paulcarmichael/fincrypt/html/")
+
+	if r.URL.String() == "/" {
+		buffer.WriteString("index")
+	} else {
+		buffer.WriteString(r.URL.String())
+	}
+
+	buffer.WriteString(".html")
+
+	// recover and parse the template file
+	file, err := filepath.Abs(buffer.String())
+
+	if err != nil {
+		log.Fatal("Abs: ", err)
+	}
+
+	t, err := template.ParseFiles(file)
+
+	// if the template wasn't found then prepare the 404 page
+	if err != nil {
+		log.Println("ParseFiles: ", err)
+
+		file, err = filepath.Abs("../src/github.com/paulcarmichael/fincrypt/html/404.html")
+
+		if err != nil {
+			log.Fatal("Abs: ", err)
+		}
+
+		t, err = template.ParseFiles(file)
+
+		if err != nil {
+			log.Fatal("ParseFiles: ", err)
+		}
+
+		w.WriteHeader(http.StatusNotFound)
+	}
 
 	t.Execute(w, nil)
 }
 
-type xorOperation struct {
-	Input1 string
-	Input2 string
-	Result string
-}
+func faviconHandler(w http.ResponseWriter, r *http.Request) {
+	file, err := filepath.Abs("../src/github.com/paulcarmichael/fincrypt/images/favicon.ico")
 
-func xor(w http.ResponseWriter, r *http.Request) {
-
-	// if the method is GET then we have a request for the xor page
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("C:\\Users\\Paul\\go\\src\\github.com\\paulcarmichael\\fincrypt\\html\\xor.html")
-
-		t.Execute(w, nil)
-	} else {
-		// parse the input variables and store them in the result structure
-		r.ParseForm()
-
-		xorResult := xorOperation{
-			Input1: strings.Join(r.Form["input1"], ""),
-			Input2: strings.Join(r.Form["input2"], ""),
-		}
-
-		// pack the given inputs
-		packedI1, err := cryptop.PackFromHex(xorResult.Input1)
-
-		if err != nil {
-			log.Print("xor failed to pack input1: ", xorResult.Input1)
-			log.Print(err)
-		}
-
-		packedI2, err := cryptop.PackFromHex(xorResult.Input2)
-
-		if err != nil {
-			log.Print("xor failed to pack input2: ", xorResult.Input2)
-			log.Print(err)
-		}
-
-		// xor
-		result, err := cryptop.XOR(string(packedI1),
-			string(packedI2))
-
-		if err != nil {
-			log.Print(err)
-			log.Print("xor failed, see earlier log messages")
-		}
-
-		// expand the result to the correct form for the frontend
-		xorResult.Result, err = cryptop.ExpandToHex(result)
-
-		log.Print("xor result [" + xorResult.Result + "]")
-
-		// generate the response
-		t, _ := template.ParseFiles("C:\\Users\\Paul\\go\\src\\github.com\\paulcarmichael\\fincrypt\\html\\xor.html")
-
-		t.Execute(w, xorResult)
+	if err != nil {
+		log.Fatal("Abs: ", err)
 	}
-}
 
-type aesOperation struct {
-	Key    string
-	Data   string
-	IV     string
-	Result string
-}
+	_, err = os.Stat(file)
 
-func aes(w http.ResponseWriter, r *http.Request) {
-
-	// if the method is GET then we have a request for the aes page
-	if r.Method == "GET" {
-		t, _ := template.ParseFiles("C:\\Users\\Paul\\go\\src\\github.com\\paulcarmichael\\fincrypt\\html\\aes.html")
-
-		t.Execute(w, nil)
-	} else {
-		// parse the input variables and store them in the result structure
-		r.ParseForm()
-
-		aesResult := aesOperation{
-			Key:  strings.Join(r.Form["key"], ""),
-			Data: strings.Join(r.Form["data"], ""),
-			IV:   strings.Join(r.Form["iv"], ""),
-		}
-
-		// pack the given inputs
-		packedKey, err := cryptop.PackFromHex(aesResult.Key)
-
-		if err != nil {
-			log.Print("aes failed to pack key: ", aesResult.Key)
-			log.Print(err)
-		}
-
-		packedData, err := cryptop.PackFromHex(aesResult.Data)
-
-		if err != nil {
-			log.Print("aes failed to pack data: ", aesResult.Data)
-			log.Print(err)
-		}
-
-		packedIV, err := cryptop.PackFromHex(aesResult.IV)
-
-		if err != nil {
-			log.Print("aes failed to pack iv: ", aesResult.IV)
-			log.Print(err)
-		}
-
-		// prepare the operation mode
-		mode := cryptop.Encrypt
-
-		if r.Form.Get("mode") == "decrypt" {
-			mode = cryptop.Decrypt
-		}
-
-		// aes
-		result, err := cryptop.AES_CBC(packedKey,
-			packedData,
-			packedIV,
-			mode)
-
-		if err != nil {
-			log.Print(err)
-			log.Print("aes failed, see earlier log messages")
-		}
-
-		// expand the result to the correct form for the frontend
-		aesResult.Result, err = cryptop.ExpandToHex(result)
-
-		log.Print("aes result [" + aesResult.Result + "]")
-
-		// generate the response
-		t, _ := template.ParseFiles("C:\\Users\\Paul\\go\\src\\github.com\\paulcarmichael\\fincrypt\\html\\aes.html")
-
-		t.Execute(w, aesResult)
+	if err != nil {
+		log.Fatal("Stat: ", err)
 	}
+
+	http.ServeFile(w, r, file)
 }
 
 func main() {
-
 	// start the logger
 	logFile, err := os.OpenFile("fincrypt.log",
 		os.O_WRONLY|os.O_CREATE|os.O_APPEND,
@@ -168,9 +86,8 @@ func main() {
 	log.Println("fincrypt starting...")
 
 	// register the handler functions
-	http.HandleFunc("/", index)
-	http.HandleFunc("/xor", xor)
-	http.HandleFunc("/aes", aes)
+	http.HandleFunc("/", serve)
+	http.HandleFunc("/favicon.ico", faviconHandler)
 
 	// go to work!
 	err = http.ListenAndServe(":8080", nil)
