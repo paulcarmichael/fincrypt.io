217fba5fe7160ccde60862e53b4c5e39b12929bd
Paul Carmichael <paulcarmo@gmail.com>
2019-02-16 16:50:13 -0800

    Moving to ajax calls to calculate results

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,11 @@
 FROM golang:alpine
 
+# git is required for fetching dependencies
+RUN apk update && apk add --no-cache git
+
 COPY . /go/src/github.com/paulcarmichael/fincrypt_http
 
+RUN go get -d -v /go/src/github.com/paulcarmichael/fincrypt_http
 RUN go install -v /go/src/github.com/paulcarmichael/fincrypt_http
 
 ENTRYPOINT /go/bin/fincrypt_http
--- main.go
+++ main.go
@@ -2,59 +2,148 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 	"path/filepath"
+
+	"github.com/paulcarmichael/fincrypt"
 )
 
+type operationResponse struct {
+	Res string
+	Err string
+}
+
 func serve(w http.ResponseWriter, r *http.Request) {
-	// build the file path of the template we require based on the request URL
-	var buffer bytes.Buffer
+	if r.Method == http.MethodGet {
+		// if the request method is get, it's a request for a webpage
+		var buffer bytes.Buffer
+		buffer.WriteString("/go/src/github.com/paulcarmichael/fincrypt_http/html")
 
-	buffer.WriteString("/go/src/github.com/paulcarmichael/fincrypt_http/html")
+		if r.URL.String() == "/" {
+			buffer.WriteString("/index")
+		} else {
+			buffer.WriteString(r.URL.String())
+		}
+		buffer.WriteString(".html")
 
-	if r.URL.String() == "/" {
-		buffer.WriteString("/index")
-	} else {
-		buffer.WriteString(r.URL.String())
-	}
+		log.Println(http.MethodGet, buffer.String())
 
-	buffer.WriteString(".html")
-
-	// recover and parse the template file
-	file, err := filepath.Abs(buffer.String())
-
-	if err != nil {
-		log.Println("Abs: ", err)
-		return
-	}
-
-	t, err := template.ParseFiles(file)
-
-	// if the template wasn't found then prepare the 404 page
-	if err != nil {
-		log.Println("ParseFiles: ", err)
-
-		file, err = filepath.Abs("/go/src/github.com/paulcarmichael/fincrypt_http/html/404.html")
+		// recover and parse the template file
+		file, err := filepath.Abs(buffer.String())
 
 		if err != nil {
 			log.Println("Abs: ", err)
+			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		t, err = template.ParseFiles(file)
+		t, err := template.ParseFiles(file)
 
+		// if the template wasn't found then prepare the 404 page
 		if err != nil {
 			log.Println("ParseFiles: ", err)
+
+			file, err = filepath.Abs("/go/src/github.com/paulcarmichael/fincrypt_http/html/404.html")
+
+			if err != nil {
+				log.Println("Abs: ", err)
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			t, err = template.ParseFiles(file)
+
+			if err != nil {
+				log.Println("ParseFiles: ", err)
+				w.WriteHeader(http.StatusNotFound)
+				return
+			}
+
+			w.WriteHeader(http.StatusNotFound)
+		}
+
+		t.Execute(w, nil)
+	} else if r.Method == http.MethodPost {
+		// if the request method is post, it's a request for a calculation
+		var op fincrypt.Operation
+		var response operationResponse
+
+		url := r.URL.String()
+
+		if url == "/aes" {
+			op = &fincrypt.AESOperation{}
+		} else if url == "/base64" {
+			op = &fincrypt.Base64Operation{}
+		} else if url == "/bertlv" {
+			op = &fincrypt.BERTLVParser{}
+		} else if url == "/cvv" {
+			op = &fincrypt.CVVOperation{}
+		} else if url == "/des" {
+			op = &fincrypt.DESOperation{}
+		} else if url == "/luhn" {
+			op = &fincrypt.LuhnOperation{}
+		} else if url == "/pinoffset" {
+			op = &fincrypt.PINOffsetOperation{}
+		} else if url == "/pvv" {
+			op = &fincrypt.PVVOperation{}
+		} else if url == "/retailmac" {
+			op = &fincrypt.RetailMACOperation{}
+		} else if url == "/xor" {
+			op = &fincrypt.XOROperation{}
+		} else {
+			log.Println("Unknown URL:", url)
+			w.WriteHeader(http.StatusNotFound)
 			return
 		}
 
-		w.WriteHeader(http.StatusNotFound)
-	}
+		// unpack the json payload into the operation struct
+		body, err := ioutil.ReadAll(r.Body)
 
-	t.Execute(w, nil)
+		if err != nil {
+			log.Println("ReadAll", err.Error())
+			w.WriteHeader(http.StatusNotAcceptable)
+			return
+		}
+
+		log.Println(http.MethodPost, url, string(body))
+
+		err = json.Unmarshal(body, &op)
+
+		if err != nil {
+			log.Println("Unmarshal", err.Error())
+			w.WriteHeader(http.StatusNotAcceptable)
+			return
+		}
+
+		// calculate the result of the operation
+		response.Res, err = op.Calculate()
+
+		if err != nil {
+			response.Err = err.Error()
+		}
+
+		// build the response
+		if len(response.Err) > 0 {
+			w.WriteHeader(http.StatusNotAcceptable)
+		}
+
+		responseBytes, err := json.Marshal(response)
+
+		if err != nil {
+			log.Println("Marshal", err.Error())
+			w.WriteHeader(http.StatusNotAcceptable)
+		}
+
+		log.Println(http.MethodPost, url, string(responseBytes))
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(responseBytes)
+	}
 }
 
 func fileHandler(w http.ResponseWriter, r *http.Request) {
